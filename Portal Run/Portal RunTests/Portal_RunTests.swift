2f37131aee51119805a8254f9fad9e72bd388bca
Phat Pham <phatpham@berkeley.edu>
2016-11-11 10:34:08 -0800

    Added Files

---
+++ Portal Run/Portal RunTests/Portal_RunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Portal_RunTests.swift
+//  Portal RunTests
+//
+//  Created by Phat Pham on 11/11/16.
+//  Copyright © 2016 Portal Run Team. All rights reserved.
+//
+
+import XCTest
+@testable import Portal_Run
+
+class Portal_RunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
